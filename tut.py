e0564079b277fa3fafb97c83c72339530598f8b6
gitcrusher <aayushguptasoni@gmail.com>
2025-03-02 12:28:02 +0530

    commit

--- tut.py
+++ tut.py
@@ -3,8 +3,9 @@ from tkinter import *
 root  = Tk()
 
 #creating  a Label widgets
-myLabel = Label(root, text="Hello World!")
 #shoving it onto the screen
+myLabel = Label(root, text="Hello World!")
+
 myLabel.pack()
 
 root.mainloop()
